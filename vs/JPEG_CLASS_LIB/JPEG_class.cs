1692654c85b396889f4b4d86508f37ac0c32bdc4
Andrey Shedlbauer <shedlbauera@mail.ru>
2020-09-18 13:59:27 +0300

    Добавил xml комментарии для библиотеки.

--- vs/JPEG_CLASS_LIB/JPEG_class.cs
+++ vs/JPEG_CLASS_LIB/JPEG_class.cs
@@ -1,32 +1,60 @@
 using System;
 using System.IO;
 
+/// <summary>
+/// Библиотека предназначена для сжатия и распаковки изображений в формате JPEG.
+/// </summary>
 public class JPEG_CS
 {
+    /// <summary>
+    /// Ширина изображения.
+    /// </summary>
 	int width;
+    /// <summary>
+    /// Высота изображения.
+    /// </summary>
 	int height;
 	
+    /// <summary>
+    /// Создает объект из потока.
+    /// </summary>
+    /// <param name="name">Поток для создания объекта.</param>
 	public JPEG_CS(Stream name)
 	{
 		
 	}
 	
+    /// <summary>
+    /// Распаковывает содержимое JPEG и возвращает изображение.
+    /// </summary>
+    /// <returns></returns>
 	public Point[,] UnPack()
 	{
 		return new Point[1,1];
 	}
 	
+    /// <summary>
+    /// Сжимает изображение и записывает его в поток.
+    /// </summary>
+    /// <param name="picture">Изображение.</param>
 	public void Compress(Point[,] picture)
 	{
 		
 	}
 	
+    /// <summary>
+    /// Устанавливает параметры сжатия JPEG.
+    /// </summary>
+    /// <param name="parameters">Параметр сжатия JPEG.</param>
 	public void SetArguments(int parameters)
 	{
 		
 	}
 }
 
+/// <summary>
+/// Параметры сжатия JPEG.
+/// </summary>
 public enum Arguments
 {
 	HIGH_QUALITY = 1 << 0,
@@ -35,9 +63,21 @@ public enum Arguments
 	ENTROPY_QUALITY = 1 << 3
 }
 
+/// <summary>
+/// Точка.
+/// </summary>
 public struct Point
-	{
-		public byte r;
-		public byte g;
-		public byte b;
-	}
+{
+    /// <summary>
+    /// Красный byte.
+    /// </summary>
+    public byte r;
+    /// <summary>
+    /// Зеленый byte.
+    /// </summary>
+    public byte g;
+    /// <summary>
+    /// Синий byte.
+    /// </summary>
+    public byte b;
+}
