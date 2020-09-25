e1836b9e8dd3b820d0fad40a983e0685a5b6524a
Vas1leps <vasilepsmagica@gmail.com>
2020-09-25 13:05:20 +0300

    Создание графического теста

---
+++ iss3/GUITest/GUITest/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GUITest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {            
+            JPEG_CS Test = new JPEG_CS(File.Open("testjpeg.jpg", FileMode.Create));
+            Point[,] CurrentJPEG = Test.UnPack();
+            int width = CurrentJPEG.GetLength(0);
+            int lenth = CurrentJPEG.GetLength(1);
+            Bitmap BM = new Bitmap(width, lenth);
+            for (int i = 0; i < width; i++)
+                for (int j = 0; j < lenth; j++) BM.SetPixel(i, j, Color.FromArgb(CurrentJPEG[i, j].r, CurrentJPEG[i, j].g, CurrentJPEG[i, j].b));            
+            InitializeComponent();
+            pictureBox1.Size = BM.Size;
+            pictureBox1.Image = BM;         
+        }
+    }
+}
