3eacea2981b16e9fcbcd4dd5d2ce60bc8466a978
Dmitriy14 <dima.morozov-2016@yandex.ru>
2020-10-09 13:58:44 +0300

    Класс DCT методы FDCT и IDCT

--- test/test_jpeg/Program.cs
+++ test/test_jpeg/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using JPEG_CLASS_LIB;
 
 namespace ConsoleApp1
 {
@@ -9,6 +10,8 @@ namespace ConsoleApp1
         {
             _TestPack();
             _TestUnpack();
+            _TestDCT();
+            
         }
 
         static void _TestUnpack()
@@ -31,6 +34,49 @@ namespace ConsoleApp1
                 }
             }
             p.Compress(points);
+        }
+        static void _TestDCT()
+        {
+            short[,]matrix= new short[8, 8];
+            //short a = 0;
+            Random r = new Random();
+
+            for (int i = 0; i<8;i++)
+            {
+                for (int j=0; j<8;j++)
+                {
+                    matrix[i, j] = Convert.ToInt16(Convert.ToInt16(r.Next(-128,128)));
+                    if (i==0) matrix[i, j] = Convert.ToInt16(Convert.ToInt16(0));
+                    if (j == 0) matrix[i, j] = Convert.ToInt16(Convert.ToInt16(0));
+                    if (i == 7) matrix[i, j] = Convert.ToInt16(Convert.ToInt16(0));
+                    if (j == 7) matrix[i, j] = Convert.ToInt16(Convert.ToInt16(0));
+
+                    Console.Write(matrix[i, j]+" ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            short[,] matrix3 = DCT.FDCT(matrix);
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+
+                    Console.Write(matrix3[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            short[,]matrix2= DCT.IDCT(matrix3);
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    
+                    Console.Write(matrix2[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
---
+++ vs/JPEG_CLASS_LIB/DCT.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JPEG_CLASS_LIB
+{
+    public class DCT
+    {
+        //public byte[,] matrix;
+        public static short[,] FDCT(short[,]inp_matrix)
+        {
+            short[,] matrix = inp_matrix;
+            int n = matrix.GetLength(0);
+            int m = matrix.GetLength(1);
+            short[,] matrix2 = new short[n, m];
+            double S = 0;
+            double Cv = 1;
+            double Cu = 1;
+            for (int v = 0; v < n; v++)
+            {
+                //if (u == 0) Cu = 1 / Math.Sqrt(2);
+                //else Cu = 1;
+                for (int u = 0; u < m; u++)
+                {
+                    S = 0;
+                    for (int x = 0; x < m; x++)
+                    {
+                        for (int y = 0; y < n; y++)
+                        {
+                            S += matrix[y, x] * Math.Cos((2 * x + 1) * u * Math.PI / 16.0) * Math.Cos((2 * y + 1) * v * Math.PI / 16.0);
+                        }
+                    }
+                    if ((v == 0) && (u == 0)) { Cv = 1 / Math.Sqrt(2); Cu = 1 / Math.Sqrt(2); }
+                    else { Cv = 1; Cu = 1; }
+                    matrix2[v, u] = Convert.ToInt16(S*Cu * Cv/4.0);
+                    //matrix2[u, v] = Convert.ToInt16(S);
+                    //matrix2[u, v] = (S * Cu * Cv / 4);
+
+                }
+            }
+            return(matrix2);
+        }
+
+
+        public static short[,] IDCT(short[,] inp_matrix)
+        {
+            short[,] matrix = inp_matrix;
+            int n = matrix.GetLength(0);
+            int m = matrix.GetLength(1);
+            short[,] matrix2 = new short[n, m];
+            double s = 0;
+            double Cv = 1;
+            double Cu = 1;
+            for (int x = 0; x < m; x++)
+            {
+                //if (u == 0) Cu = 1 / Math.Sqrt(2);
+                //else Cu = 1;
+                for (int y = 0; y < n; y++)
+                {
+                    s = 0;
+                    for (int u = 0; u < m; u++)
+                    {
+                        for (int v = 0; v < n; v++)
+                        {
+                            if ((v == 0) && (u == 0)) { Cv = 1 / Math.Sqrt(2); Cu = 1 / Math.Sqrt(2); }
+                            else { Cv = 1; Cu = 1; }
+                            s += Cu*Cv*(matrix[v, u] * Math.Cos((2 * x + 1) * u * Math.PI / 16.0) * Math.Cos((2 * y + 1) * v * Math.PI / 16.0));
+                        }
+                    }
+                    //if (v == 0) Cv = 1 / Math.Sqrt(2);
+                    //else Cv = 1;
+                    matrix2[y, x] = Convert.ToInt16(s/4.0);
+                    //matrix2[y, x] = (s / 4);
+
+                }
+            }
+            return (matrix2);
+        }
+    }
+}
