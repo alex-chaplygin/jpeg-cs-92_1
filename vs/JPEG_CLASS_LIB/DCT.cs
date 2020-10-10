ec562d161a6a344fd755b3c6e29e94c5076e6f49
alex-chaplygin <alex_chaplygin@mail.ru>
2020-10-10 07:54:57 +0300

    Исправление ошибок и тестирование DCT

--- vs/JPEG_CLASS_LIB/DCT.cs
+++ vs/JPEG_CLASS_LIB/DCT.cs
@@ -15,28 +15,21 @@ namespace JPEG_CLASS_LIB
             double S = 0;
             double Cv = 1;
             double Cu = 1;
-            for (int v = 0; v < n; v++)
-            {
-                //if (u == 0) Cu = 1 / Math.Sqrt(2);
-                //else Cu = 1;
-                for (int u = 0; u < m; u++)
+            for (int u = 0; u < n; u++)
+                for (int v = 0; v < m; v++)
                 {
                     S = 0;
                     for (int x = 0; x < m; x++)
-                    {
                         for (int y = 0; y < n; y++)
                         {
                             S += matrix[y, x] * Math.Cos((2 * x + 1) * u * Math.PI / 16.0) * Math.Cos((2 * y + 1) * v * Math.PI / 16.0);
                         }
-                    }
-                    if ((v == 0) && (u == 0)) { Cv = 1 / Math.Sqrt(2); Cu = 1 / Math.Sqrt(2); }
-                    else { Cv = 1; Cu = 1; }
+		    Cv = 1;
+		    Cu = 1;
+                    if (u == 0)  Cu = 1 / Math.Sqrt(2);
+		    if (v == 0) Cv = 1 / Math.Sqrt(2); 
                     matrix2[v, u] = Convert.ToInt16(S*Cu * Cv/4.0);
-                    //matrix2[u, v] = Convert.ToInt16(S);
-                    //matrix2[u, v] = (S * Cu * Cv / 4);
-
                 }
-            }
             return(matrix2);
         }
 
@@ -51,28 +44,20 @@ namespace JPEG_CLASS_LIB
             double Cv = 1;
             double Cu = 1;
             for (int x = 0; x < m; x++)
-            {
-                //if (u == 0) Cu = 1 / Math.Sqrt(2);
-                //else Cu = 1;
                 for (int y = 0; y < n; y++)
                 {
                     s = 0;
                     for (int u = 0; u < m; u++)
-                    {
                         for (int v = 0; v < n; v++)
                         {
-                            if ((v == 0) && (u == 0)) { Cv = 1 / Math.Sqrt(2); Cu = 1 / Math.Sqrt(2); }
-                            else { Cv = 1; Cu = 1; }
+			    Cv = 1;
+			    Cu = 1;
+			    if (u == 0)  Cu = 1 / Math.Sqrt(2);
+			    if (v == 0) Cv = 1 / Math.Sqrt(2); 
                             s += Cu*Cv*(matrix[v, u] * Math.Cos((2 * x + 1) * u * Math.PI / 16.0) * Math.Cos((2 * y + 1) * v * Math.PI / 16.0));
                         }
-                    }
-                    //if (v == 0) Cv = 1 / Math.Sqrt(2);
-                    //else Cv = 1;
                     matrix2[y, x] = Convert.ToInt16(s/4.0);
-                    //matrix2[y, x] = (s / 4);
-
                 }
-            }
             return (matrix2);
 	}
         /// <summary>
