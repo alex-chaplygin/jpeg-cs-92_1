16a3f4641b61960ef902edfa2db920e86f6b1d75
Leticia <72256272+leticiamarsongelsch@users.noreply.github.com>
2020-10-30 14:08:01 +0300

    Update RestartInterval.cs

--- vs/JPEG_CLASS_LIB/RestartInterval.cs
+++ vs/JPEG_CLASS_LIB/RestartInterval.cs
@@ -5,16 +5,22 @@ using System.Text;
 
 namespace JPEG_CLASS_LIB
 {
+	/// <summary>
+	/// class for интервал повтора - число MCU - minimum coding unit
+	/// </summary>
 	public class RestartInterval : JPEGData
 	{
 		public ushort restartInterval;
+		//читает интервал повтора из потока
 		public RestartInterval(Stream s) : base(s)
 		{
+			//use method Read16 from JPEGData
 			restartInterval = Read16();
-		
 		}
+		//пишет интервал повтора из потока
 		public void Write()
 		{
+			//use method Write16 from JPEGData
 			Write16(restartInterval);
 		}
 	}
