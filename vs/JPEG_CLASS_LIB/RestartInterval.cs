4073b8bd30d31ce48e4b5d2422259fd2e4bc1b4b
Leticia <72256272+leticiamarsongelsch@users.noreply.github.com>
2020-10-30 13:55:30 +0300

    Create RestartInterval.cs

---
+++ vs/JPEG_CLASS_LIB/RestartInterval.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace JPEG_CLASS_LIB
+{
+	public class RestartInterval : JPEGData
+	{
+		public ushort restartInterval;
+		public RestartInterval(Stream s) : base(s)
+		{
+			restartInterval = Read16();
+		
+		}
+		public void Write()
+		{
+			Write16(restartInterval);
+		}
+	}
+}
