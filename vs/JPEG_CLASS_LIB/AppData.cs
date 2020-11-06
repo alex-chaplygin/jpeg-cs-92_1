2f45e9b8d0c54f5dd5cfc2570e15c1b14353bdd7
AfterWaLifu <maximaphonin@gmail.com>
2020-11-06 13:47:17 +0300

    В класс AppData добавлены метод Print и документация

--- vs/JPEG_CLASS_LIB/AppData.cs
+++ vs/JPEG_CLASS_LIB/AppData.cs
@@ -5,18 +5,44 @@ using System.Text;
 
 namespace JPEG_CLASS_LIB
 {
+    /// <summary>
+    /// Класс AppData - содержит данные, методы их чтения из потока и записи в поток.
+    /// </summary>
     class AppData : JPEGData
     {
+        /// <summary>
+        /// Массив данных
+        /// </summary>
         public byte[] data;
 
+        /// <summary>
+        /// Конструктор класса, считывает данные из потока
+        /// </summary>
+        /// <param name="s"></param>
         public AppData(Stream s):base(s)
         {
             s.Read(data,0,Lenght-2);
         }
 
+        /// <summary>
+        /// Метод записи массива данных в поток.
+        /// </summary>
+        /// <param name="s"></param>
         public void Write(Stream s)
         {
             s.Write(data, 0, data.Length);
+        }
+
+        /// <summary>
+        /// Метод вывода данных в консоль
+        /// </summary>
+        public void Print()
+        {
+            for (int i = 0; i < data.Length; i++)
+            {
+                Console.Write($"{data[i]}\t");
+                if (i%7 == 0) {Console.WriteLine();}
+            }
         }
     }
 }
