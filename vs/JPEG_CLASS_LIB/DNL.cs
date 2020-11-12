9cb91b7b49e3ea1f7aa09c815c98e7d8382247f8
Vas1leps <vasilepsmagica@gmail.com>
2020-11-12 21:22:00 +0300

    Добавлен DNL

---
+++ vs/JPEG_CLASS_LIB/DNL.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace JPEG_CLASS_LIB
+{
+    /// <summary>
+    /// Класс сегмента DNL.
+    /// </summary>
+    public class DNL:JPEGData
+    {
+        /// <summary>
+        /// Количество строк.
+        /// </summary>
+        public ushort NL;
+
+        /// <summary>
+        /// Создаёт экземпляр класса Define Number of Lines, считывает количество строк.
+        /// </summary>
+        /// <param name="s">Поток, на основе которого создается экземпляр класса.</param>
+        public DNL(Stream s): base (s, MarkerType.DefineNumberOfLines)
+        {
+            NL = Read16();
+        }
+
+        /// <summary>Выводит в консоль поля класса.</summary>
+        public override void Print()
+        {
+            base.Print();
+            Console.WriteLine("Количество строк в frame: " + NL);
+        }
+    }
+}
