e93639db585ad23780eb96d9ef3465c3bc6abb28
MonterBB <vadim193775@gmail.com>
2020-11-27 18:46:34 +0300

    Изменён путь к изображению JPEG_example_down в проекте модульного тестирования.

--- test/JPEG.Tests/AppDataTest.cs
+++ test/JPEG.Tests/AppDataTest.cs
@@ -9,14 +9,12 @@ namespace JPEG.Tests
     public class AppDataTest
 
     {
-        /*static string path = "../../../JPEG_example_down.jpg";
-        FileStream s = new FileStream(path, FileMode.Open);*/
         [TestMethod]
         public void TestAppData_Print()
         {
             byte[] expected = new byte[10]{74, 70, 73, 70, 0, 1, 2, 1, 0, 72};
             byte[] actual = new byte[10];
-            string path = @"E:\SWSU\КПО\jpeg-cs-92_1\test\test_jpeg\JPEG_example_down.jpg";
+            string path = "../../../test_jpeg/JPEG_example_down.jpg";
             FileStream s = new FileStream(path, FileMode.Open);
             s.Seek(0x4, SeekOrigin.Begin);
             AppData appData_Test_Print = new AppData(s);
@@ -28,7 +26,7 @@ namespace JPEG.Tests
         [TestMethod]
         public void TestAppData_Write()
         {
-            string path = @"E:\SWSU\КПО\jpeg-cs-92_1\test\test_jpeg\JPEG_example_down.jpg";
+            string path = "../../../test_jpeg/JPEG_example_down.jpg";
             byte[] Testrray = new byte[18];
             byte[] ExpectedArraytedArray = new byte[] { 0xFF, 0xE0, 00 ,16, 74, 70, 73, 70, 0, 1, 2, 1, 0, 72, 0, 72, 0, 0 };
             MemoryStream M = new MemoryStream(Testrray, true);            
