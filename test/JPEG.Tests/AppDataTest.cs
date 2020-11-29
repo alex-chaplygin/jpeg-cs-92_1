e8d5d34b140dcde931b6211739b05de89b2005f2
Valentin <playvell17@gmail.com>
2020-11-29 17:58:46 +0300

    Добавлен тест для класса DCT

--- test/JPEG.Tests/AppDataTest.cs
+++ test/JPEG.Tests/AppDataTest.cs
@@ -39,4 +39,67 @@ namespace JPEG.Tests
             CollectionAssert.AreEqual(Testrray, ExpectedArraytedArray);
         }
     }
+
+    [TestClass]
+    public class DCTTest
+    {
+        [TestMethod]
+        public void TestDCTDifference()
+        {
+            var jpeg = new JPEG_CS(null);
+            var passDifference = new [] {42, 73, 148};
+            var parametersName = new [] {"высокого качества", "среднего качества", "низкого качества"};
+            for (var parameters = 0; parameters < 3; parameters++)
+            {
+                Console.WriteLine($"Параметры: {Convert.ToInt32(Math.Pow(2, parameters))}");
+                jpeg.SetParameters(Convert.ToInt32(Math.Pow(2, parameters)));
+                var width = 8;
+                var height = 8;
+                var testMatrix = new byte[width, height];
+                var r = new Random();
+                for (var i = 0; i < height; i++)
+                {
+                    for (var j = 0; j < width; j++)
+                    {
+                        testMatrix[j, i] = (byte) r.Next(0, 255);
+                    }
+                }
+                var channel = new Channel(testMatrix, 2, 2);
+                var split = channel.Split();
+                Console.WriteLine("Тестовая матрица:");
+                WriteMatrix(split[0]);
+                var fdct = jpeg.FDCT(split);
+                var idct = jpeg.IDCT(fdct);
+                Console.WriteLine("Матрица после DCT:");
+                WriteMatrix(idct[0]);
+                Console.WriteLine("Разностная матрица:");
+                var absDifference = 0;
+                for (int y = 0; y < split[0].GetLength(1); y++)
+                {
+                    for (int x = 0; x < split[0].GetLength(0); x++)
+                    {
+                        var tmpDiff = split[0][x, y] - idct[0][x, y];
+                        Assert.IsTrue(tmpDiff < passDifference[parameters], $"Разница ({tmpDiff}) превышает допустимые показатели для {parametersName[parameters]}");
+                        absDifference += Math.Abs(tmpDiff);
+                        Console.Write(Convert.ToString(tmpDiff).PadRight(3, ' ')+"| ");
+                    }
+                    Console.WriteLine();
+                }
+                Console.WriteLine($"Абсолютная разность: {absDifference}");
+                Console.WriteLine();
+            }
+        }
+                
+        static void WriteMatrix(byte[,] matrix)
+        {
+            for (int y = 0; y < matrix.GetLength(1); y++)
+            {
+                for (int x = 0; x < matrix.GetLength(0); x++)
+                {
+                    Console.Write(matrix[x, y].ToString("X2") + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
 }
