75fca806d9286baf2994279cb6051f43ace56c20
AfterWaLifu <maximaphonin@gmail.com>
2020-12-04 14:47:40 +0300

    заставил работать первый тест

--- test/JPEG.Tests/DecodingTest.cs
+++ test/JPEG.Tests/DecodingTest.cs
@@ -1,28 +1,27 @@
 ﻿using System;
-using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using JPEG_CLASS_LIB;
 using System.IO;
+using JPEG_CLASS_LIB;
 
 namespace JPEG.Tests
 {
     [TestClass]
-    class DecodingTest
+    public class DecodingTest
     {
         [TestMethod]
-        private static void _TestDecodingNextBit()
+        public void _TestDecodingNextBit()
         {
             byte[] expected = new byte[5];
             byte[] actual = new byte[5];
 
             Random r = new Random();
-            FileStream S = File.Open("../../../test.jpg", FileMode.Open);
+            //FileStream S = File.Open("../../../test.jpg", FileMode.Open);
 
-            S.Seek(0x1f7, SeekOrigin.Begin);
-            HuffmanTable huffDC = new HuffmanTable(S);
-            S.Seek(0x218, SeekOrigin.Begin);
-            HuffmanTable huffAC = new HuffmanTable(S);
-            S.Seek(0x3a7, SeekOrigin.Begin); //начала скана
+            //S.Seek(0x1f7, SeekOrigin.Begin);
+            HuffmanTable huffDC = null;
+            //S.Seek(0x218, SeekOrigin.Begin);
+            HuffmanTable huffAC = null;
+            //S.Seek(0x3a7, SeekOrigin.Begin); //начала скана
 
             MemoryStream ms = new MemoryStream(5);
             for (int i = 0; i < 5; i++)
@@ -32,7 +31,7 @@ namespace JPEG.Tests
             }
             ms.Seek(0, SeekOrigin.Begin);
 
-            Decoding d = new Decoding(ms, huffDC, huffAC);
+            JPEG_CLASS_LIB.Decoding d = new JPEG_CLASS_LIB.Decoding(ms, huffDC, huffAC);
 
             for (int i = 0; i < 5; i++)
             {
@@ -45,12 +44,12 @@ namespace JPEG.Tests
             }
 
             CollectionAssert.AreEqual(expected, actual);
-            S.Dispose();
+            //S.Dispose();
             ms.Dispose();
         }
 
         [TestMethod]
-        private static void _TestDecodingRecieve()
+        public static void _TestDecodingRecieve()
         {
             ushort[] expected = new ushort[5];
             ushort[] actual = new ushort[5];
@@ -67,12 +66,12 @@ namespace JPEG.Tests
             MemoryStream ms = new MemoryStream(5);
             for (int i = 0; i < 5; i++)
             {
-                expected[i] = (byte)r.Next(0, (int)Math.Pow(2,16));
-                ms.Write(new byte[2] {(byte)(expected[i] >> 8),(byte)(expected[i] % 256) },0,2);
+                expected[i] = (byte)r.Next(0, (int)Math.Pow(2, 16));
+                ms.Write(new byte[2] { (byte)(expected[i] >> 8), (byte)(expected[i] % 256) }, 0, 2);
             }
             ms.Seek(0, SeekOrigin.Begin);
 
-            Decoding d = new Decoding(ms, huffDC, huffAC);
+            JPEG_CLASS_LIB.Decoding d = new JPEG_CLASS_LIB.Decoding(ms, huffDC, huffAC);
 
             for (int i = 0; i < 5; i++)
             {
@@ -85,7 +84,7 @@ namespace JPEG.Tests
         }
 
         [TestMethod]
-        private static void _TestDecodingExtend()
+        public static void _TestDecodingExtend()
         {
             short expected = 0;
 
@@ -119,20 +118,20 @@ namespace JPEG.Tests
             }
             ms.Seek(0, SeekOrigin.Begin);
 
-            Decoding d = new Decoding(ms, huffDC, huffAC);
+            JPEG_CLASS_LIB.Decoding d = new JPEG_CLASS_LIB.Decoding(ms, huffDC, huffAC);
 
             ushort actual = d.Receive(8);
-            actual = (ushort)Decoding.Extend(actual, 8);
+            actual = (ushort)JPEG_CLASS_LIB.Decoding.Extend(actual, 8);
             Console.Write($"Результат: {actual}");
 
             ushort temp = (ushort)Math.Pow(2, 7);
             while (expected < temp)
             {
-                temp = (ushort)((-1*Math.Pow(2,8))+1);
+                temp = (ushort)((-1 * Math.Pow(2, 8)) + 1);
                 expected += (short)temp;
             }
 
-            Assert.AreEqual(expected,actual);
+            Assert.AreEqual(expected, actual);
             S.Dispose();
             ms.Dispose();
         }
