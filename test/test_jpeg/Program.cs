a5a8a103925c5939f8690dca66a9fde415e7792e
Dmitriy14 <dima.morozov-2016@yandex.ru>
2020-12-05 18:38:18 +0300

    Удалил из program тестирование DecodingACDC

--- test/test_jpeg/Program.cs
+++ test/test_jpeg/Program.cs
@@ -29,44 +29,11 @@ namespace ConsoleApp1
             // _TestDCTcoding();
             //_TestChannelV2();
             // _TestEncodingWriteBits();*/
-            _TestDecodingDCAC();
+            
             Console.ReadKey();
         }
 
-        static void _TestDecodingDCAC()
-        {
-            short[] Block = new short[64];
-            FileStream S = File.Open("../../../test.jpg", FileMode.Open);
-            S.Seek(0x1f7, SeekOrigin.Begin);
-            HuffmanTable huffDC = new HuffmanTable(S);
-            S.Seek(0x218, SeekOrigin.Begin);
-            HuffmanTable huffAC = new HuffmanTable(S);
-            S.Seek(0x3b3, SeekOrigin.Begin);
-            Decoding decoding = new Decoding(S, huffDC, huffAC);
-            for (int k = 0; k < 10; k++)
-            {
-                decoding.huffDC = huffDC;
-                decoding.huffDC.GenerateTables();
-                Block[0] = decoding.DecodeDC();
-                decoding.huffAC = huffAC;
-                decoding.huffAC.GenerateTables();
-                decoding.DecodeAC(Block);
-                Console.WriteLine("Декодирование DC и AC");
-                for (int i = 0, j = 1; i < 64; i++, j++)
-                {
-                    string s = Block[i].ToString();
-                    while (s.Length < 5) s = " " + s;
-                    Console.Write(s);
-                    if (j == 8)
-                    {
-                        Console.WriteLine();
-                        j = 0;
-                    }
-                }
-            }
-            Console.WriteLine();
-            S.Dispose();
-        }
+        
 
         static void _TestChannelV2()
         {
