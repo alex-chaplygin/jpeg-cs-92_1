594f9f52c1b63a2ea262571c75317821c6afb3a1
MonterBB <vadim193775@gmail.com>
2020-12-10 17:45:49 +0300

    Перенесены поля класса и добавлен метод PrintData в класс JPEGfile

--- vs/JPEG_CLASS_LIB/JPEGFile.cs
+++ vs/JPEG_CLASS_LIB/JPEGFile.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
-using System.Text;
 
 namespace JPEG_CLASS_LIB
 {
@@ -13,40 +11,7 @@ namespace JPEG_CLASS_LIB
         /// <summary>
         /// Список всех структур JPEG до StartOfScan.
         /// </summary>
-        List<JPEGData> Data = new List<JPEGData>{};
-
-        /// <summary>
-        /// Конструктор JPEGFile. Считывает все структуры JPEGData и записывает их в Data.
-        /// </summary>
-        /// <param name="s">Поток с изображением.</param>
-        public JPEGFile(Stream s)
-        {
-            long position = 0;
-            do
-            {
-                position = s.Position;
-                var temp = JPEGData.GetData(s);
-                s.Position = position + temp.Length + 2;
-                if (temp.Marker == MarkerType.DefineHuffmanTables) huffmanTables.Add((HuffmanTable)temp);
-                else if (temp.Marker >= MarkerType.BaseLineDCT && temp.Marker <= MarkerType.DifferentialLoslessArithmetic) frame = (Frame)temp;
-                else if (temp.Marker == MarkerType.DefineQuantizationTables) quantizationTables.Add((QuantizationTable)temp);                
-                else if (temp.Marker == MarkerType.StartOfScan) scan = (Scan)temp;
-                Data.Add(temp);
-            }
-            while (Data[Data.Count - 1].Marker != MarkerType.StartOfScan);
-            decoding = new Decoding(s, null, null);
-        }
-
-        /// <summary>
-        /// Выводит в консоль все JPEGData
-        /// </summary>
-        public void Print()
-        {
-            foreach(JPEGData d in Data)
-            {
-                d.Print();
-            }
-        }
+        List<JPEGData> Data = new List<JPEGData> { };
 
         /// <summary>
         /// Кадр
@@ -72,5 +37,46 @@ namespace JPEG_CLASS_LIB
         /// Класс декодирования энтропийных данных.
         /// </summary>
         Decoding decoding;
+
+        /// <summary>
+        /// Конструктор JPEGFile. Считывает все структуры JPEGData и записывает их в Data.
+        /// </summary>
+        /// <param name="s">Поток с изображением.</param>
+        public JPEGFile(Stream s)
+        {
+            long position = 0;
+            do
+            {
+                position = s.Position;
+                var temp = JPEGData.GetData(s);
+                s.Position = position + temp.Length + 2;
+                if (temp.Marker == MarkerType.DefineHuffmanTables) huffmanTables.Add((HuffmanTable)temp);
+                else if (temp.Marker >= MarkerType.BaseLineDCT && temp.Marker <= MarkerType.DifferentialLoslessArithmetic) frame = (Frame)temp;
+                else if (temp.Marker == MarkerType.DefineQuantizationTables) quantizationTables.Add((QuantizationTable)temp);
+                else if (temp.Marker == MarkerType.StartOfScan) scan = (Scan)temp;
+                Data.Add(temp);
+            }
+            while (Data[Data.Count - 1].Marker != MarkerType.StartOfScan);
+            decoding = new Decoding(s, null, null);
+        }
+
+        /// <summary>
+        /// Выводит в консоль все JPEGData
+        /// </summary>
+        public void Print()
+        {
+            foreach (JPEGData d in Data)
+            {
+                d.Print();
+            }
+        }
+
+        public void PrintData()
+        {
+            frame.Print();
+            foreach (JPEGData d in quantizationTables) d.Print();
+            foreach (JPEGData d in huffmanTables) d.Print();
+            scan.Print();
+        }
     }
 }
