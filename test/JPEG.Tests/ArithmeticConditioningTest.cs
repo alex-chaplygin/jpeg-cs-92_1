7152a123e71ec7f333b79232a983f96f4f8ac099
Andrey Shedlbauer <shedlbauerand@gmail.com>
2020-12-11 19:22:21 +0300

    Добавлен модульный тест ArithmeticConditioningTest

---
+++ test/JPEG.Tests/ArithmeticConditioningTest.cs
@@ -0,0 +1,44 @@
+﻿using JPEG_CLASS_LIB;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+
+namespace JPEG.Tests
+{
+    [TestClass]
+    public class ArithmeticConditioningTest
+    {
+        [TestMethod]
+        public void ArithmeticConditioning()
+        {
+            ushort Length = 0x1;
+
+            Multiple[] multiples = {
+                new Multiple((byte)0x0, (byte)0x1, (byte)0x05), 
+                new Multiple((byte)0x6, (byte)0xA, (byte)0xF3),
+                new Multiple((byte)0xB, (byte)0xF, (byte)0xFF)};
+
+            ushort LA = (ushort)(2 + multiples.Length * 2);
+            byte[] CodesArray = new byte[2 + 2 + multiples.Length * 2];
+
+            CodesArray[0] = (byte)(Length >> 8);
+            CodesArray[1] = (byte)(Length & 0xFF);
+
+            CodesArray[2] = (byte)(LA >> 8);
+            CodesArray[3] = (byte)(LA & 0xFF);
+
+            for (int i = 0; i < multiples.Length; i++)
+            {
+                CodesArray[4 + i * 2] = (byte)((multiples[i].Tc << 4) + multiples[i].Tb);
+                CodesArray[4 + i*2 + 1] = multiples[i].Cs;
+            }
+
+            MemoryStream M = new MemoryStream(CodesArray, true);
+            ArithmeticConditioning arithmeticConditioning = new ArithmeticConditioning(M);
+            Console.WriteLine("Результат чтения arithmeticConditioning:");
+            arithmeticConditioning.Print();
+
+            M.Dispose();
+        }
+    }
+}
