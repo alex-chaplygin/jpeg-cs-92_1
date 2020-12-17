7ae6cf058edd90d11fc3979e42590c0dcbb191c3
MonterBB <vadim193775@gmail.com>
2020-12-17 14:23:57 +0300

    Реализовал методы GetHuffmanTable, GetQuantizationTable, GetRestartInterval в классе JPEGFile.

--- vs/JPEG_CLASS_LIB/JPEGFile.cs
+++ vs/JPEG_CLASS_LIB/JPEGFile.cs
@@ -39,25 +39,71 @@ namespace JPEG_CLASS_LIB
         Decoding decoding;
 
         /// <summary>
+        /// Интервал повтора - количество MCU - минимальных кодированных блоков. 
+        /// </summary>
+        RestartInterval restartInterval;
+
+        /// <summary>
         /// Конструктор JPEGFile. Считывает все структуры JPEGData и записывает их в Data.
         /// </summary>
         /// <param name="s">Поток с изображением.</param>
         public JPEGFile(Stream s)
         {
-            long position = 0;
             do
             {
-                position = s.Position;
+                long position = s.Position;
                 var temp = JPEGData.GetData(s);
                 s.Position = position + temp.Length + 2;
                 if (temp.Marker == MarkerType.DefineHuffmanTables) huffmanTables.Add((HuffmanTable)temp);
                 else if (temp.Marker >= MarkerType.BaseLineDCT && temp.Marker <= MarkerType.DifferentialLoslessArithmetic) frame = (Frame)temp;
                 else if (temp.Marker == MarkerType.DefineQuantizationTables) quantizationTables.Add((QuantizationTable)temp);
                 else if (temp.Marker == MarkerType.StartOfScan) scan = (Scan)temp;
+                else if (temp.Marker == MarkerType.DefineRestartInterval) restartInterval = (RestartInterval)temp;
                 Data.Add(temp);
             }
             while (Data[Data.Count - 1].Marker != MarkerType.StartOfScan);
             decoding = new Decoding(s, null, null);
+        }
+
+        /// <summary>
+        /// Ищет таблицу Хаффмана с заданными параметрами
+        /// </summary>
+        /// <param name="cl">Класс таблицы (0 - DC, 1 - AC)</param>
+        /// <param name="num">Номер таблицы Хаффмана</param>
+        /// <returns>Структура фрейма</returns>
+        public HuffmanTable GetHuffmanTable(byte cl, byte num)
+        {
+            HuffmanTable result = null;
+            for (int i = 0; i < huffmanTables.Count; i++)
+                if (cl == huffmanTables[i].Tc && num == huffmanTables[i].Th)
+                    result = huffmanTables[i];
+            return result;
+        }
+
+        /// <summary>
+        /// Находит структуру таблицы квантования
+        /// </summary>
+        /// <param name="num">Номер таблицы квантования</param>
+        /// <returns>матрица квантования</returns>
+        public short[,] GetQuantizationTable(int num)
+        {
+            short[,] result = new short[8, 8];
+            for (int x = 0; x < 8; x++)
+                for (int y = 0; y < 8; y++)
+                    result[y, x] = quantizationTables[num].QuantizationTableMain[x + 8 * y];
+            return result;
+        }
+
+        /// <summary>
+        /// Возращает интервал повтора (количество MCU - минимальных кодированных блоков)
+        /// </summary>
+        /// <returns>интервал повтора</returns>
+        public int GetRestartInterval()
+        {
+            if (restartInterval != null)
+                return restartInterval.restartInterval;
+            else
+                return 0;
         }
 
         /// <summary>
