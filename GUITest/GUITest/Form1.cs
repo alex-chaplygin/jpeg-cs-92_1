e1aaa11017a618120c8218c6236d320af9b6dd52
Vas1leps <vasilepsmagica@gmail.com>
2021-01-24 11:27:32 +0300

    Изменен путь к файлу

--- GUITest/GUITest/Form1.cs
+++ GUITest/GUITest/Form1.cs
@@ -1,14 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
+﻿using System.Drawing;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using JPEG_CLASS_LIB;
 
 
 namespace GUITest
@@ -16,17 +8,17 @@ namespace GUITest
     public partial class Form1 : Form
     {
         public Form1()
-        {            
-            JPEG_CS Test = new JPEG_CS(File.Open("testjpeg.jpg", FileMode.Open));
+        {
+            JPEG_CS Test = new JPEG_CS(File.Open("../../../../test/test_jpeg/test.jpg", FileMode.Open));
             Point[,] CurrentJPEG = Test.UnPack();
             int width = CurrentJPEG.GetLength(0);
             int lenth = CurrentJPEG.GetLength(1);
             Bitmap BM = new Bitmap(width, lenth);
             for (int i = 0; i < width; i++)
-                for (int j = 0; j < lenth; j++) BM.SetPixel(i, j, Color.FromArgb(CurrentJPEG[i, j].r, CurrentJPEG[i, j].g, CurrentJPEG[i, j].b));            
+                for (int j = 0; j < lenth; j++) BM.SetPixel(i, j, Color.FromArgb(CurrentJPEG[i, j].r, CurrentJPEG[i, j].g, CurrentJPEG[i, j].b));
             InitializeComponent();
             pictureBox1.Size = BM.Size;
-            pictureBox1.Image = BM;         
+            pictureBox1.Image = BM;
         }
     }
 }
